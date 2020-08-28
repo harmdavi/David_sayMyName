b45b2e0d94cf58af61790ad03eb3de2d839c7a61
harmdavi <harmondrum@gmail.com>
2020-08-27 21:08:44 -0600

    Add project files.

---
+++ David_sayMyName.vb
@@ -0,0 +1,13 @@
+﻿Option Strict On
+Option Explicit On
+
+
+
+Module David_sayMyName
+
+    Sub Main()
+        Console.WriteLine("Hello There! What is your name")
+        Console.ReadLine()
+    End Sub
+
+End Module

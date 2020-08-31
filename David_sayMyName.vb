59f28a244bac0dff9df4516165bfd2405b173187
harmdavi <harmondrum@gmail.com>
2020-08-30 18:56:53 -0600

    Figuring out submission thing

--- David_sayMyName.vb
+++ David_sayMyName.vb
@@ -1,12 +1,31 @@
-﻿Option Strict On
-Option Explicit On
+﻿'David Harmon
+'RCET0265
+'Fall 2020
+'Say My Name
+'https://github.com/rosstimo/MyRepo.git
 
+
+Option Strict On
+Option Explicit On
+Option Compare Binary
 
 
 Module David_sayMyName
 
     Sub Main()
-        Console.WriteLine("Hello There! What is your name")
+
+        Dim userResponce As String
+        Console.WriteLine("Hello There! What is your name?")
+        userResponce = Console.ReadLine()
+        If userResponce = "Say my name" Then
+            Console.WriteLine("umm... come again?")
+            userResponce = Console.ReadLine()
+        End If
+        If userResponce = "Say my name" Then
+            Console.WriteLine("Heisenberg")
+        Else Console.WriteLine("Well hello there " & userResponce & "!")
+
+        End If
         Console.ReadLine()
     End Sub
 

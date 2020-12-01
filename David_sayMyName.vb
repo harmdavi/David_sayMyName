b972925f3280eeee16fa74a54559dbc4143f0657
harmdavi <harmondrum@gmail.com>
2020-12-01 14:10:33 -0700

    Made an exit option. also added a loop so that the program will continue running until the q is pressed

--- David_sayMyName.vb
+++ David_sayMyName.vb
@@ -2,7 +2,7 @@
 'RCET0265
 'Fall 2020
 'Say My Name
-'https://github.com/rosstimo/MyRepo.git
+'https://github.com/harmdavi/David_sayMyName.git
 
 
 Option Strict On
@@ -13,20 +13,29 @@ Option Compare Binary
 Module David_sayMyName
 
     Sub Main()
-
         Dim userResponce As String
-        Console.WriteLine("Hello There! What is your name?")
-        userResponce = Console.ReadLine()
-        If userResponce = "Say my name" Then
-            Console.WriteLine("umm... come again?")
+        Do Until userResponce = "q"
+            Console.WriteLine("Hello There! What is your name?")
             userResponce = Console.ReadLine()
-        End If
-        If userResponce = "Say my name" Then
-            Console.WriteLine("Heisenberg")
-        Else Console.WriteLine("Well hello there " & userResponce & "!")
+            If userResponce = "Say my name" Then
+                Console.WriteLine("umm... come again?")
+                userResponce = Console.ReadLine()
+            ElseIf userResponce = "q" Then
+                Exit Sub
+            End If
+            If userResponce = "Say my name" Then
+                Console.WriteLine("Heisenberg")
+            ElseIf userResponce = "q" Then
+                Exit Sub
+            Else Console.WriteLine("Well hello there " & userResponce & "!" & vbNewLine & "Press Q to Exit")
 
-        End If
-        Console.ReadLine()
+            End If
+            If Console.ReadLine() = "q" Then
+                Exit Sub
+            End If
+            Console.Clear()
+        Loop
     End Sub
+
 
 End Module
